f2962d84737e440285468909a63f9df68ae13352
yan <yan@realbaselive.com>
2020-02-19 11:26:00 +1300

    Added loads of headers and move the testing demo loop to the main.js file

--- src/js/drawing.js
+++ src/js/drawing.js
@@ -1,13 +1,23 @@
 "use strict";
 
 /*
-Drawing namespace
+ ____                     _
+|  _ \ _ __ __ ___      _(_)_ __   __ _   _ __   __ _ _ __ ___   ___  ___ _ __   __ _  ___ ___
+| | | | '__/ _` \ \ /\ / / | '_ \ / _` | | '_ \ / _` | '_ ` _ \ / _ \/ __| '_ \ / _` |/ __/ _ \
+| |_| | | | (_| |\ V  V /| | | | | (_| | | | | | (_| | | | | | |  __/\__ \ |_) | (_| | (_|  __/
+|____/|_|  \__,_| \_/\_/ |_|_| |_|\__, | |_| |_|\__,_|_| |_| |_|\___||___/ .__/ \__,_|\___\___|
+                                  |___/                                  |_|
 */
 var Drawing = {}
 
 
 /*
-Drawing.Pixel Class
+ ____                     _               ____  _          _    ____ _
+|  _ \ _ __ __ ___      _(_)_ __   __ _  |  _ \(_)_  _____| |  / ___| | __ _ ___ ___
+| | | | '__/ _` \ \ /\ / / | '_ \ / _` | | |_) | \ \/ / _ \ | | |   | |/ _` / __/ __|
+| |_| | | | (_| |\ V  V /| | | | | (_| |_|  __/| |>  <  __/ | | |___| | (_| \__ \__ \
+|____/|_|  \__,_| \_/\_/ |_|_| |_|\__, (_)_|   |_/_/\_\___|_|  \____|_|\__,_|___/___/
+                                  |___/
 */
 Drawing.Pixel = function (r, g, b) {
 	// fields
@@ -93,7 +103,12 @@ Drawing.Pixel.Average = function (xs) { // static method to average a list of pi
 
 
 /*
-Drawing.Canvas Class
+ ____                     _               ____                             ____ _
+|  _ \ _ __ __ ___      _(_)_ __   __ _  / ___|__ _ _ ____   ____ _ ___   / ___| | __ _ ___ ___
+| | | | '__/ _` \ \ /\ / / | '_ \ / _` || |   / _` | '_ \ \ / / _` / __| | |   | |/ _` / __/ __|
+| |_| | | | (_| |\ V  V /| | | | | (_| || |__| (_| | | | \ V / (_| \__ \ | |___| | (_| \__ \__ \
+|____/|_|  \__,_| \_/\_/ |_|_| |_|\__, (_)____\__,_|_| |_|\_/ \__,_|___/  \____|_|\__,_|___/___/
+                                  |___/
 */
 Drawing.Canvas = function (selector, scale) {
 	// process
@@ -150,7 +165,12 @@ Drawing.Canvas.FramesPerSecond = 2;
 
 
 /*
-Drawing.Frame Class
+ ____                     _               _____                            ____ _
+|  _ \ _ __ __ ___      _(_)_ __   __ _  |  ___| __ __ _ _ __ ___   ___   / ___| | __ _ ___ ___
+| | | | '__/ _` \ \ /\ / / | '_ \ / _` | | |_ | '__/ _` | '_ ` _ \ / _ \ | |   | |/ _` / __/ __|
+| |_| | | | (_| |\ V  V /| | | | | (_| |_|  _|| | | (_| | | | | | |  __/ | |___| | (_| \__ \__ \
+|____/|_|  \__,_| \_/\_/ |_|_| |_|\__, (_)_|  |_|  \__,_|_| |_| |_|\___|  \____|_|\__,_|___/___/
+                                  |___/
 */
 Drawing.Frame = function (width, height, pixelGrid) {
 	// validate
@@ -177,44 +197,3 @@ Drawing.Frame.prototype.getPixelAt = function (x, y) {
 		return new Drawing.Pixel();
 	}
 }
-
-var canvas;
-
-$(window).on("load", function () {
-	canvas = new Drawing.Canvas("#canvas", 8)
-
-	var randomSurface = function () {
-		return [
-			new Drawing.Pixel(
-				Math.random() * 255,
-				Math.random() * 255,
-				Math.random() * 255
-			)
-		];
-	}
-
-	// draw the space border
-	for (var x = 0; x < canvas.space.getWidth(); x++) {
-		for (var y = 0; y < canvas.space.getLength(); y++) {
-			if (y === 0 || y === canvas.space.getLength() - 1 || x === 0 || x == canvas.space.getWidth() - 1) {
-				canvas.space.drawPoint(x, y, randomSurface());
-			}
-	 	}
-	}
-
-	// draw some rectangles
-	canvas.space.drawRectangle(5, 25, 9, 72, randomSurface());
-	canvas.space.drawRectangle(15, 75, 19, 92, randomSurface());
-	canvas.space.drawRectangle(25, 15, 29, 48, randomSurface());
-	canvas.space.drawRectangle(95, 5, 36, 43, randomSurface());
-	canvas.space.drawRectangle(95, 75, 56, 93, randomSurface());
-
-	// draw some circles
-	canvas.space.drawCircle(30, 70, 10, randomSurface())
-
-	setInterval(function () {
-		canvas.drawFrame(new Drawing.Frame(Math.round(canvas.getWidth() / canvas.getScale()),
-		                                   Math.round(canvas.getHeight() / canvas.getScale()),
-		                                   canvas.space.map()))
-	}, 1000 / Drawing.Canvas.FramesPerSecond);
-});
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,21 @@
+/*
+  ____ _       _           _
+ / ___| | ___ | |__   __ _| |___
+| |  _| |/ _ \| '_ \ / _` | / __|
+| |_| | | (_) | |_) | (_| | \__ \
+ \____|_|\___/|_.__/ \__,_|_|___/
+
+*/
+var canvas;
+
+/*
+ ___       _ _   _       _ _          _   _
+|_ _|_ __ (_) |_(_) __ _| (_)______ _| |_(_) ___  _ __
+ | || '_ \| | __| |/ _` | | |_  / _` | __| |/ _ \| '_ \
+ | || | | | | |_| | (_| | | |/ / (_| | |_| | (_) | | | |
+|___|_| |_|_|\__|_|\__,_|_|_/___\__,_|\__|_|\___/|_| |_|
+
+*/
 $(document).ready(function () {
 	// wire up color scheme
 	$("body, #outer")
@@ -6,14 +24,59 @@ $(document).ready(function () {
 		.addClass("border-normal");
 })
 
+/*
+__     __    _     _   __  __       _          __                    __
+\ \   / /__ (_) __| | |  \/  | __ _(_)_ __    / /__ _ _ __ __ ___   _\ \
+ \ \ / / _ \| |/ _` | | |\/| |/ _` | | '_ \  | |/ _` | '__/ _` \ \ / /| |
+  \ V / (_) | | (_| | | |  | | (_| | | | | | | | (_| | | | (_| |\ V / | |
+   \_/ \___/|_|\__,_| |_|  |_|\__,_|_|_| |_| | |\__,_|_|  \__, | \_/  | |
+                                              \_\         |___/      /_/
+*/
 $(window).on("load", function () {
 	var makeFullScreen = function () {
 		$("#outter")
 			.width($(window).width())
 			.height($(window).height());
 	};
-	
+
 	// make the outer wrapper fullscreen and responsive
 	makeFullScreen();
 	$(window).on("resize", makeFullScreen);
+
+	canvas = new Drawing.Canvas("#canvas", 8)
+
+	var randomSurface = function () {
+		return [
+			new Drawing.Pixel(
+				Math.random() * 255,
+				Math.random() * 255,
+				Math.random() * 255
+			)
+		];
+	}
+
+	// draw the space border
+	for (var x = 0; x < canvas.space.getWidth(); x++) {
+		for (var y = 0; y < canvas.space.getLength(); y++) {
+			if (y === 0 || y === canvas.space.getLength() - 1 || x === 0 || x == canvas.space.getWidth() - 1) {
+				canvas.space.drawPoint(x, y, randomSurface());
+			}
+	 	}
+	}
+
+	// draw some rectangles
+	canvas.space.drawRectangle(5, 25, 9, 72, randomSurface());
+	canvas.space.drawRectangle(15, 75, 19, 92, randomSurface());
+	canvas.space.drawRectangle(25, 15, 29, 48, randomSurface());
+	canvas.space.drawRectangle(95, 5, 36, 43, randomSurface());
+	canvas.space.drawRectangle(95, 75, 56, 93, randomSurface());
+
+	// draw some circles
+	canvas.space.drawCircle(30, 70, 10, randomSurface())
+
+	setInterval(function () {
+		canvas.drawFrame(new Drawing.Frame(Math.round(canvas.getWidth() / canvas.getScale()),
+		                                   Math.round(canvas.getHeight() / canvas.getScale()),
+		                                   canvas.space.map()))
+	}, 1000 / Drawing.Canvas.FramesPerSecond);
 });
