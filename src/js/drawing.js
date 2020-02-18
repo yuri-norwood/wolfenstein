8c3419d0940a425ae45c53beaa00a2551d5293b2
yan <yan@realbaselive.com>
2020-02-18 14:43:40 +1300

    Add some rectangles to the testing space.

--- src/js/drawing.js
+++ src/js/drawing.js
@@ -23,15 +23,28 @@ $(window).on("load", function () {
 	var x_unit = canvas.width / 100;
 	var y_unit = canvas.height / 100;
 	var framesPerSec = 2;
+	var defaultSurface = [
+		new Raytracing.Pixel(
+			Math.random() * 255,
+			Math.random() * 255,
+			Math.random() * 255
+		)
+	];
 
 	space = new Raytracing.Space(100, 100, 100);
 	for (var x = 0; x < space.getWidth(); x++) {
 		for (var y = 0; y < space.getLength(); y++) {
-			if (y === 0 || y === space.getLength() - 1 || x === 0 || x == space.getWidth()) {
-				space.drawPoint(x, y, [new Raytracing.Pixel(Math.random() * 255, Math.random() * 255, Math.random() * 255)]);
+			if (y === 0 || y === space.getLength() - 1 || x === 0 || x == space.getWidth() - 1) {
+				space.drawPoint(x, y, defaultSurface);
 			}
 		}
 	}
+
+	space.drawRectangle(5, 25, 9, 72, defaultSurface);
+	space.drawRectangle(15, 75, 19, 92, defaultSurface);
+	space.drawRectangle(25, 15, 29, 42, defaultSurface);
+	space.drawRectangle(95, 5, 36, 43, defaultSurface);
+	space.drawRectangle(95, 75, 56, 93, defaultSurface);
 
 	// setInterval(function () {
 	// 	space.viewPoint.turnLeft();
