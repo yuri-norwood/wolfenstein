8d1d984e729e95607b2f902104869aac0b21a143
Yuri Norwood <76186754+yuri-norwood@users.noreply.github.com>
2020-02-21 14:59:34 +1300

    Start migrating everything into web workers

---
+++ src/js/wolfenstein.js
@@ -0,0 +1,24 @@
+"use strict";
+
+/*
+ __        __    _  __                _       _         _____ ____
+ \ \      / /__ | |/ _| ___ _ __  ___| |_ ___(_)_ __   |___ /|  _ \
+  \ \ /\ / / _ \| | |_ / _ \ '_ \/ __| __/ _ \ | '_ \    |_ \| | | |
+   \ V  V / (_) | |  _|  __/ | | \__ \ ||  __/ | | | |  ___) | |_| |
+    \_/\_/ \___/|_|_|  \___|_| |_|___/\__\___|_|_| |_| |____/|____/
+*/
+
+
+/*
+ Imports
+*/
+importScripts("raytracing.js");
+
+/*
+ Functions
+*/
+function init() { // initalise game - this is the entry point
+	
+}
+
+init(); // start the game
